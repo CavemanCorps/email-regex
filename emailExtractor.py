f9712214323f513e74b413c07f50429e4016a88b
RomanRenders <103530319+RomanRenders@users.noreply.github.com>
2022-05-17 23:24:30 -0700

    Add files via upload

---
+++ emailExtractor.py
@@ -0,0 +1,22 @@
+#! python3
+# emailExtractor.py - Simplified version of extractorProject.py. Only finds emails.
+
+import pyperclip, re
+
+emailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+       # username
+    @                       # @ symbol
+    [a-zA-Z0-9.-]+          # domain name
+    (\.[a-zA-Z]{2,4})       # dot-something
+    )''', re.VERBOSE)
+
+# Finds matches in the Clipboard 
+text = str(pyperclip.paste())                               
+matches = []
+for groups in emailRegex.findall(text):                    
+    matches.append(groups[0])                              
+
+# Copies matches from clipboard                 
+pyperclip.copy('\n'.join(matches))
+print('WE FOUND '+ str(len(matches)) + ' MATCHES')
+print('\n'.join(matches))
